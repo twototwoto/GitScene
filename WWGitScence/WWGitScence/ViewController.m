a0bef7d9b4f986699f52ad7180e17f7143564638
twototwoto <2637405168@qq.com>
2018-08-19 19:52:24 +0800

    更新dev的说明文字要merge到master

--- WWGitScence/WWGitScence/ViewController.m
+++ WWGitScence/WWGitScence/ViewController.m
@@ -27,7 +27,11 @@
     NSLog(@"添加Dev代码");
     /**
      * 观察提交后是否会影响到master的内容
-     * 两个分支在commit内容后直接切换 不会影响到彼此的内容 
+     * 两个分支在commit内容后直接切换 不会影响到彼此的内容
+     * 刚才master出现了超前两次提交的情况 push一次后还提示 要求我们push
+     * 直接点击了下SourceTree的push 这次的测试重点不在这里 先不看这里
+     * 考虑下此时的dev的更改然后再commit 一次
+     * 然后merge 到master
      */
 }
 
