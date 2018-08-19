2c7cc291473e3d53c21cfd61169dae45ba2a5755
twototwoto <2637405168@qq.com>
2018-08-19 18:30:04 +0800

    简化代码

---
+++ WWGitScence/WWGitScence/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WWGitScence
+//
+//  Created by wangyongwang on 2018/8/19.
+//  Copyright © 2018年 ITCoderW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WWGitScence/WWGitScence/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  WWGitScence
+//
+//  Created by wangyongwang on 2018/8/19.
+//  Copyright © 2018年 ITCoderW. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+
+@end
