0468756b00619585393570a43d40413b1ea29f2e
twototwoto <2637405168@qq.com>
2018-08-19 19:48:32 +0800

    改动master中的说明文字

--- WWGitScence/WWGitScence/ViewController.m
+++ WWGitScence/WWGitScence/ViewController.m
@@ -27,7 +27,11 @@
     NSLog(@"写一点master Codes");
     /**
      * dev 的commit后切换回master 发现dev改变的内容并没有影响到master
-     * 
+     * master也commit了一次 因为不commit的话 不允许checkout 到其他分支
+     * 出现了master超前一个版本的问题 这个时候根据现象来看
+     * 是参照着master来看本地和远端的情况的
+     * 接下来在master上push上去代码 看看怎么把dev merge到master
+     * 因为此时master和dev 的代码改动部分是有冲突的 看看接下来的解决
      
      */
 }
