2b1fec2476b31318eedfd8e0c9865ce14e2a7bb5
twototwoto <2637405168@qq.com>
2018-10-19 11:34:33 +0800

    maste 提交内容

---
+++ WWGitScence/WWGitScence/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  WWGitScence
+//
+//  Created by wangyongwang on 2018/10/19.
+//  Copyright © 2018年 ITCoderW. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WWGitScence/WWGitScence/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  WWGitScence
+//
+//  Created by wangyongwang on 2018/10/19.
+//  Copyright © 2018年 ITCoderW. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
